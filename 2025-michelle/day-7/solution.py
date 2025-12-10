788fb4a566fc842cf3db0920464240ac7653f855
Michelle Lam <mlam8428@gmail.com>
2025-12-09 23:07:56 -0500

    Add day 7

--- 2025-michelle/day-7/solution.py
+++ 2025-michelle/day-7/solution.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 BASE_DIR = os.path.dirname(__file__)
 # input_path = os.path.join(BASE_DIR, "sample-input.txt")
@@ -18,7 +19,7 @@ class Solution:
         first_row = self.tree[0]
         return first_row.index('S')
 
-    def run(self):
+    def run_part1(self):
         num_split = 0
         for i in range(1, len(self.tree)):
             curr_tree_level = self.tree[i]
@@ -36,6 +37,30 @@ class Solution:
             
             self.beams = list(new_beams)
         return num_split
+    
+    def run_part2(self):
+        @lru_cache(maxsize=None)
+        def count_timelines(row, col):
+            # If we’ve left the manifold, that's one completed timeline
+            if row >= len(self.tree) or col < 0 or col >= len(self.tree[row]):
+                return 1
+
+            cell = self.tree[row][col]
+
+            if cell == "^":
+                # Split: particle’s timeline forks into left and right
+                left = count_timelines(row + 1, col - 1)
+                right = count_timelines(row + 1, col + 1)
+                return left + right
+            else:
+                # Empty / start / anything-not-^ : just fall straight down
+                return count_timelines(row + 1, col)
+
+        # Beam starts in the row *below* S, same as in part 1
+        start_row = 1
+        return count_timelines(start_row, self.get_start())
+
 
 s = Solution()
-print(s.run()) # 1560
+# print("Part 1:", s.run_part1())  # 1560
+print("Part 2:", s.run_part2())
